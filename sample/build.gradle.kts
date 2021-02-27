e95eefbedb6eb8831265b10397d488535f4c564a
Vaclav Drahorad <drahorad.vac@gmail.com>
2021-02-27 22:18:05 +0100

    Kotlinize dependencies

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -30,12 +30,11 @@ android {
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation(project(":file-save"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.0")
-    implementation("androidx.core:core-ktx:1.3.2")
-    implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation(KotlinDependencies.kotlin)
+    implementation(AndroidDependencies.androidxCore)
+    implementation(AndroidDependencies.appCompat)
 
-    testImplementation("junit:junit:4.12")
-    androidTestImplementation("androidx.test.ext:junit:1.1.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+    testImplementation(TestDependencies.junit)
+    androidTestImplementation(TestDependencies.androidxTest)
+    androidTestImplementation(TestDependencies.espresso)
 }
