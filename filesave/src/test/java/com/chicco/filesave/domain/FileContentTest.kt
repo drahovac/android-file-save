0b67b33c6fd84d24219ee2fe17134655890b4824
Václav Drahorád <vaclav.drahorad@cleverlance.com>
2021-06-18 14:20:39 +0200

    Add file content test

---
+++ filesave/src/test/java/com/chicco/filesave/domain/FileContentTest.kt
@@ -0,0 +1,25 @@
+package com.chicco.filesave.domain
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+
+@RunWith(JUnit4::class)
+class FileContentTest {
+
+    private companion object {
+        const val ANY_NAME = "ANY_NAME"
+        const val ANY_SUFFIX = "ANY_SUFFIX"
+        val ANY_FILE_CONTENT = FileContentTestBuilder(
+            fileNameWithoutSuffix  = ANY_NAME,
+            suffix = ANY_SUFFIX
+        ).build()
+    }
+
+    @Test
+    fun `should return properly formatted name with suffix`() {
+        assertEquals("ANY_NAME.ANY_SUFFIX", ANY_FILE_CONTENT.fileNameWithSuffix)
+    }
+}
